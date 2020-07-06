7e86e6aed502f93825e3de06f340c09e6744c363
Yahir Keymurth <arzkaner@gmail.com>
2020-07-06 16:53:08 -0500

    Se implementa el cambio de la rama prueba

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.log("Hola soy lo principal de la rama master");
 console.log("No afecto a master");
-console.log("AHORA SI NO TENGO QUE AFECTAR A MASTER")
+console.log("AHORA SI NO TENGO QUE AFECTAR A MASTER");
